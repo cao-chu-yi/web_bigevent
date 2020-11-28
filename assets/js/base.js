854209b00a819cdbc6c15c79e40293c0f668a562
初一 <2219397044@qq.com>
2020-11-28 15:36:57 +0800

    完成主要功能的开发

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,4 +3,21 @@
 $.ajaxPrefilter(function(options) {
     // 在发起请求之前获取根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+    // 统一为有权限的接口发起headers请求
+    // 首先判断是否需要发起请求头  options.url.indexOf('/my/') !== -1判断是否包含/my
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token' || '')
+        }
+    }
+    // 无论成功和失败都会调用complete
+    options.complete = function(res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 强制清空token
+            localStorage.removeItem('token');
+            // 强制跳转
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+// 渲染用户头像
+$(function() {
+    var layer = layui.layer;
+    getuserInform();
+    // 给退出的绑定事件
+    $('#t-login').on('click', function() {
+        // console.log(12);
+        // 提示用户是否退出
+        layer.confirm('是否退出登录?', { icon: 3, title: '提示' }, function(index) {
+            // 确定退出，执行清空本地存储，跳转到登录页面
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+            // 这是关闭询问框
+            layer.close(index);
+        });
+    })
+
+});
+// 获取用户的基本信息
+function getuserInform() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token' || '')
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('用户获取失败')
+            }
+            console.log(res);
+            renderavatar(res.data);
+        }
+    })
+}
+
+function renderavatar(user) {
+    // 用户名称采取优先级
+    var name = user.nickname || user.username;
+    // 设置文本
+    $('.welcome').html('欢迎&nbsp&nbsp' + name);
+    // 渲染用户头像
+    if (user.user_pic !== null) {
+        // 如果不为空，则渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        // 如果为空，则渲染文本头像
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+
+    }
+
+}
